16450f12d4474a26b7b8f42c451aa60d998c9619
jakub <alcolp96@gmail.com>
2021-12-22 18:56:59 +0100

    Refactor 2

--- src/redux/amountSlice.ts
+++ src/redux/amountSlice.ts
@@ -5,18 +5,14 @@ export const amountSlice = createSlice({
   name: 'amount',
   initialState: {
     value: 0,
-    fetching: true,
   },
   reducers: {
     setAmountValue: (state, loadValue) => {
       state.value = loadValue.payload;
     },
-    toggleAmountFetching: (state) => {
-      state.fetching = !state.fetching;
-    }
   }
 })
 
-export const { setAmountValue, toggleAmountFetching } = amountSlice.actions;
+export const { setAmountValue } = amountSlice.actions;
 
 export default amountSlice.reducer;
--- src/redux/termSlice.ts
+++ src/redux/termSlice.ts
@@ -5,18 +5,14 @@ export const termSlice = createSlice({
   name: 'term',
   initialState: {
     value: 0,
-    fetching: true,
   },
   reducers: {
     setTermValue: (state, loadValue) => {
       state.value = loadValue.payload;
     },
-    toggleTermFetching: (state) => {
-      state.fetching = !state.fetching;
-    }
   }
 })
 
-export const { setTermValue, toggleTermFetching } = termSlice.actions;
+export const { setTermValue } = termSlice.actions;
 
 export default termSlice.reducer;
